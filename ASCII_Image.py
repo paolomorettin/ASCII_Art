da4421262d2a25c87d5de175656b2be2a05f5c1e
paolomorettin <morettin.paolo@gmail.com>
2014-11-22 16:47:25 +0100

    ASCII videos for humanity

---
+++ ASCII_Image.py
@@ -0,0 +1,110 @@
+
+import Image,ImageDraw,ImageFont
+
+class ASCII_Image :
+    greyscale_ascii = [' ','.',"'",'"','-',':','~','=','+','x','%','$','H','Z','X','#','@']
+    #greyscale_ascii = [' ','.',"'",'"','-',':','+','x','%','H','#','@']
+
+    @staticmethod
+    def openImage(path) :
+        '''
+        This just wraps "Image.open"
+
+        '''
+        return Image.open(path)
+
+    @staticmethod
+    def saveImage(image,path) :
+        '''
+        Another wrapper for saving images.
+
+        '''
+        image.save(path)
+
+    @staticmethod
+    def text_to_image(frame,width,fontsize = None,fg_color=(255,255,255),bg_color=(0,0,0)) :
+        '''
+        Converts an ASCII-art frame into a PIL Image. If no fontsize is passed, it determines the best-fitting
+        size with a binary search. Returns the image and the fontsize for future uses.
+        
+        Params:
+        frame - an ASCII-art frame
+        width - the desired image width (in pixels). (Note: height is automatically determined)
+        (OPTIONAL)
+        fontsize - this parameter can be used to compute the best-fitting size just once.
+        fg_color - text color in BGR.
+        bg_color - background color in BGR.
+
+        '''
+        
+
+        if fontsize == None :
+            # initial font size
+            bounds = [1, width * 2]
+            fontsize = bounds[0] + int((bounds[1]-bounds[0])/2.)
+            font = ImageFont.truetype("./data/DroidSansMono.ttf", fontsize)
+            # calculate the right font size to fit the image width, using a binary search
+            first_row = ''.join(frame[0])
+            while bounds[1] - bounds[0] > 2 :
+                if font.getsize(first_row)[0] > width :
+                    bounds[1] = fontsize
+                else :
+                    bounds[0] = fontsize
+                fontsize = bounds[0] + int((bounds[1]-bounds[0])/2.)
+                font = ImageFont.truetype("./data/DroidSansMono.ttf", fontsize)
+        else :
+            font = ImageFont.truetype("./data/DroidSansMono.ttf", fontsize)
+
+        height = len(frame) * fontsize
+        image = Image.new("RGB", (width,height), bg_color)
+        draw = ImageDraw.Draw(image)
+        
+        for i in range(len(frame)) :
+            draw.text((0, i*fontsize), ''.join(frame[i]), fg_color, font=font)        
+
+        return image,fontsize
+
+    @classmethod
+    def greyscale_process(cls,image,hDef,vDef,inverse=False,normalize=False) :
+        '''
+        This converts a PIL image into a matrix (list of lists) of ASCII characters, selected according to the
+        average greyscale value of each corresponing region.
+        
+        Params:
+        image - the image to be processed (PIL Image type)
+        hDef - how many characters for each line
+        vDef - how many lines
+        (OPTIONAL)
+        inverse - boolean, reverse greyscale value
+        normalize - boolean, normalize the averaged grayscale values
+
+        '''
+        matrix = []
+        width,height = image.size
+        h_gap = width/hDef
+        v_gap = height/vDef
+
+        for i in xrange(vDef) :
+            matrix.append([])
+            for j in xrange(hDef) :
+                gs_value = 0
+                for x in range(j*h_gap,(j+1)*h_gap) :
+                    for y in range(i*v_gap,(i+1)*v_gap) :                        
+                        gs_value += sum(image.getpixel( (x,y) )[:3])/3
+                matrix[-1].append( gs_value / (v_gap * h_gap) )
+
+        gs_list = list(cls.greyscale_ascii)
+
+        if inverse :
+            gs_list.reverse()
+
+        if normalize :
+            gs_min = min([min(r) for r in matrix])
+            gs_max = max([max(r) for r in matrix])
+            if gs_max > 0 :
+                matrix = [map(lambda x : int((float(x - gs_min)/gs_max)*255) , r) for r in matrix]
+        alen = len(gs_list)
+        return map(lambda row : map(lambda elem : gs_list[int((elem/256.)*alen)] , row), matrix)
+
+
+
---
+++ ASCII_Video.py
@@ -0,0 +1,109 @@
+import Image
+import numpy
+import cv2
+from ASCII_Image import ASCII_Image
+from time import sleep
+
+class ASCII_Video :
+
+    @staticmethod
+    def ascii_to_video(frames,path,width,fps,fg_color=(255,255,255),bg_color=(0,0,0),codec='XVID') :
+        '''
+        Given a list of ASCII-art frames, produces a video.
+        
+        Params:
+        frames - list of ASCII-art frames.
+        path - of the output file
+        width - desired video width (in pixels)
+        fps - desired framerate
+        (OPTIONAL)
+        fg_color - BGR values for the text
+        bg_color - BGR values for the background
+        codec - string representing the codec to use (see http://fourcc.org/codecs.php)
+
+        '''
+        image,fontsize = ASCII_Image.text_to_image(frames[0],width,fg_color=fg_color,bg_color=bg_color)
+
+        fourcc = cv2.cv.CV_FOURCC(*codec)
+        video = cv2.VideoWriter(path, fourcc, fps, image.size)
+        image = numpy.array(image)
+        video.write(image)
+        
+        i = 1
+        for f in frames[1:] :
+            i += 1
+            image,_ = ASCII_Image.text_to_image(f,width,fontsize=fontsize,fg_color=fg_color,bg_color=bg_color)
+            image = numpy.array(image)
+            video.write(image)
+            print "{}/{}".format(str(i),str(len(frames)))
+
+        # is it useful?
+        #video.release()
+
+    @staticmethod
+    def dump(path,frames) :
+        '''
+        Writes a list of ASCII-art frames as a textfile.
+
+        Params:
+        path - of the output file
+        frames - the list
+
+        '''
+        with open(path,'w') as f :
+            s_frames = []
+            for frame in frames :
+                s_frame = ''
+                for row in frame :
+                    s_frame += ''.join(row) + '\n'
+                s_frames.append(s_frame)
+            f.write('\n'.join(s_frames))
+
+    @staticmethod
+    def open(path) :
+        '''
+        Reads a textfile containing a list of ASCII-art frames, as written by the dump.
+
+        Params:
+        path - of the input file
+
+        '''
+        with open(path,'r') as frame_file :
+            http://fourcc.org/codecs.phpframes = []
+            for f in frame_file.read().split('\n\n') :
+                frames.append(map(list,f[:-1].split('\n')))
+            return frames
+                
+    @staticmethod
+    def getVideoFrames(path,step=1,preprocess=False) : 
+        # NOTE: Online preprocessing of each frame is advised. This should be implemented in order to deal with big video files.
+        '''
+        Extracts a list of frames (PIL Images) from a video file.
+
+        Params:
+        path - of the input video file
+        (OPTIONAL)
+        step - takes one frame every 'step' frames
+
+        '''
+        vc = cv2.VideoCapture(path)
+        c=0
+        frames = []
+
+        if vc.isOpened():
+            rval , frame = vc.read()
+            frames.append(frame)
+        else:
+            rval = False
+        while rval:
+             rval, frame = vc.read()
+             if c % step == 0 and rval :                    
+                 frames.append(Image.fromarray(frame))
+             c = c + 1
+        vc.release()
+        return frames
+
+
+
+
+
